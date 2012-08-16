e0bbbfb2330eccae3d285ad715f59764ca1ba412
Tony R Quilkey <thorpe@thorpesystems.com>
2012-08-16 03:48:57 -0400

    Add xhprof back into index files

--- apps/symfony2/web/app.php
+++ apps/symfony2/web/app.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_GET['debug'])) {
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
     define('START_TIME', microtime(true));
     define('START_MEMORY_USAGE', memory_get_usage());
 }
-
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
@@ -25,3 +25,17 @@ echo "Page rendered in <b>"
     . round((memory_get_usage() - START_MEMORY_USAGE) / 1024, 2) ." KB</b>";
 $f = get_included_files();
 echo ", include files: ".count($f);
+
+
+$xhprof = realpath(dirname(__FILE__)) . '/../../../lib/xhprof/xhprof_lib/utils';
+include_once $xhprof . "/xhprof_lib.php";
+include_once $xhprof . "/xhprof_runs.php";
+
+// save raw data for this profiler run using default
+// implementation of iXHProfRuns.
+$xhprof_runs = new XHProfRuns_Default();
+
+// save the run under a namespace "xhprof_foo"
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
+
+echo ", xhprof <a href=\"http://xhprof.bench/xhprof_html/index.php?run=$run_id&source=xhprof_foo\">url</a>";
