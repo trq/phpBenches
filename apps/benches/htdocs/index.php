aa22ee1f6c177772b285a88cc72670c840f922e9
Tony R Quilkey <thorpe@thorpesystems.com>
2012-09-06 03:22:49 -0400

    Various changes.

---
+++ apps/benches/htdocs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <img src="request-per-second.png" />
+    <img src="loadavg.png" />
+    <img src="memory-usage.png" />
+    <img src="number-of-files.png" />
+    <img src="number-of-function-calls.png" />
+    <img src="response-time.png" />
+  </body>
+</html>
